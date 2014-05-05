87a4f3e55a4743303fcd2d2b79a599fb20faa523
JLA <arancibiajav@gmail.com>
2014-05-05 19:24:37 -0300

    First commit !

---
+++ WebApiClarity/Controllers/ModuloController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClarityDB.T4;
+
+namespace ClarityWebApi.Controllers
+{
+    public class ModuloController : Controller
+    {
+        //
+        // GET: /Modulo/
+
+        public JsonResult Index()
+        {
+
+            SYS_Modulo module = new SYS_Modulo() {
+                Descripcion = "Modulo Ejemplo"
+            };
+
+            return Json(module, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ WebApiClarity/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ClarityWebApi.App_Start;
+
+namespace ClarityWebApi
+{
+ 
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+
+            
+
+            AreaRegistration.RegisterAllAreas();
+           // RegisterGlobalFilters(GlobalFilters.Filters);
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
