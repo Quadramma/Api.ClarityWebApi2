22e7674dbd35510833c89005fadb3b58a23a06d2
JLA <arancibiajav@gmail.com>
2014-05-16 15:10:11 -0300

    EmpresaController Added

---
+++ WebApiClarity/Controllers/EmpresaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebApiClarity.Controllers
+{
+    public class EmpresaController : ApiController
+    {
+        //
+        // GET: /Empresa/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
